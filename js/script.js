bc868ce5358d2b5aebad192706efdb34fc5da5d7
Gjelbrim Haskaj <g@gjelbr.im>
2022-12-21 09:54:49 +0100

    further improvments

--- js/script.js
+++ js/script.js
@@ -1,24 +1,22 @@
-function copyToClipboard() {
-    var copyText = document.getElementById("out");
-    copyText.select();
-    copyText.setSelectionRange(0, 99999);
-    document.execCommand("copy");
-    document.getElementById("copy").className = "";
-    document.getElementById("copy").innerHTML = "🇦🇱"
-    setTimeout(() => {
-      document.getElementById("copy").className = "fa fa-clone";
-      document.getElementById("copy").innerHTML = ""
-    }, 1000)
-  }
+const copyToClipboard = () => {
+  const outputElement = document.getElementById('out');
+  outputElement.select();
+  outputElement.setSelectionRange(0, 99999);
+  document.execCommand('copy');
+  document.getElementById('copy').className = '';
+  document.getElementById('copy').innerHTML = '🇦🇱';
+  setTimeout(() => {
+    document.getElementById('copy').className = 'fa-solid fa-clone';
+    document.getElementById('copy').innerHTML = '';
+  }, 1000);
+};
   
-  var firstTitle = false;
+  let firstTitle = false;
   
-  function changeTitle(){
-    document.title = firstTitle ? "🇦🇱 𐔇𐔐𐔁𐔀𐔛𐔀𐔓𐔛𐔂𐔙𐔍𐔗𐔝": "elbasanscri.pt 🇦🇱";
+  const toggleTitle = () => {
     firstTitle = !firstTitle;
-    setTimeout(arguments.callee, 2500);
+    document.title = firstTitle ? "🇦🇱 𐔇𐔐𐔁𐔀𐔛𐔀𐔓𐔛𐔂𐔙𐔍𐔗𐔝": "elbasanscri.pt 🇦🇱";
+    setTimeout(toggleTitle, 2500);
   }
-  
-  window.onload = function() {
-    changeTitle();
-  }
+
+  toggleTitle();
