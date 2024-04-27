76433e6969cb4917ed88d3d2ee5e8145865afbd6
Gjelbrim Haskaj <g@gjelbr.im>
2024-04-28 01:16:54 +0200

    fixed location in version bash file

--- version.sh
+++ version.sh
@@ -3,4 +3,4 @@
 GIT_BRANCH=$(git rev-parse --abbrev-ref HEAD) #current branch
 GIT_COMMIT_HASH=$(git rev-parse HEAD | cut -c 1-7) #current commit hash
 
-echo "{\"branch\": \"$GIT_BRANCH\", \"commit\": \"$GIT_COMMIT_HASH\"}" > ./public/version.json
+echo "{\"branch\": \"$GIT_BRANCH\", \"commit\": \"$GIT_COMMIT_HASH\"}" > ./version.json
